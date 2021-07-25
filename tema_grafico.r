2962d5807f2913438f3c01b0d900c210f3627ff4
Pieter Attema Zalis <31882881+Pieterzalis@users.noreply.github.com>
2021-07-25 17:58:29 -0300

    atualizando o tema

--- tema_grafico.r
+++ tema_grafico.r
@@ -1,16 +1,18 @@
-  theme_minimal() + 
+tema <- theme_minimal() +
   theme(panel.grid.minor = element_blank(),
         panel.grid.major.x = element_blank(),
         panel.grid.major.y = element_blank(),
         axis.ticks.length = unit(0, "cm"),
-        text=element_text(family='Arial'),
+        text=element_text(family='Lato'),
         plot.title = element_text(size=16,hjust=0.0,face='bold'),
-        plot.subtitle = element_text(size=14,face='italic'),
-        axis.text.x = element_text(size=16,color="black",face='bold'),
-        legend.position="top",
+        plot.subtitle = element_text(size=14,hjust=0.0,face='italic'),
+        plot.caption = element_text(size=10,face='italic',color='gray'),
+        axis.text.x = element_blank(),
+        axis.text.y = element_text(size=12,face='bold',color='black',hjust=1,vjust=0.2),
+        legend.position="none",
         legend.title = element_blank(),
+        strip.placement = "outside",
+        strip.text = element_text(size=12,face='bold',hjust = 0.5),
         legend.text = element_text(color = "black", size = 14,face='bold'),
-        axis.text.y = element_blank(),
-        axis.title.x = element_text(color="black", size=14),
-        axis.title.y = element_text(color="black", size=14)) +
-  scale_y_continuous(limits=c(-0,1700),breaks=c(500,1000,1500),expand=c(0.0,0))
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank())
